09cb0fb7aad7673e5d72f26a9e83d33431daa7da
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-04-30 10:01:59 -0500

    fix(redirect Page) : if user is logged, redirect

--- src/components/HomeOrLogin.tsx
+++ src/components/HomeOrLogin.tsx
@@ -1,41 +1,39 @@
-import React from 'react';
-
+/* React Libraries */
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router';
+/* Services */
 import { LocalStorageService } from "../services/localData-service";
-import { withIonLifeCycle } from '@ionic/react';
 
-
-class HomeOrTutorial extends React.Component {
-
-	constructor(props : any){
-		super(props)
-	}
-	ionViewWillEnter() {
-		if(true){
-			return <Redirect to="/login" />;
-		}
-    console.log('ionViewWillEnter event fired')
-	}
-	
-	render(){
- 		return <Redirect to="/slides" />;
-	}
-	// /*If User is logged then, redirect to home, if not redirect to slides tutorial*/ 
-	// const storageService = new LocalStorageService();
-	// // Check if exits jwt in the phone
-	// storageService.getItem("jwt")
-	// 	.then((token )=>{
-	// 		/* If exists the jwt in the phone the user is logged */	
-	// 		if(token){
-	// 			return <Redirect to="/Home" /> 
-
-	// 		}
-				
-	// 	})
-	// 	.catch((err)=>{
-	// 			console.log(err);
-	// 	})
-	// 	return <Redirect to="/slides" />
+/*
+	This functions checks in the local storage for a 
+	propertie "jwt" that means the user is logged in
+	if propertie exists then redirect user to home
+	if not exits redirect to slides page
+*/
+const HomeOrLogin : React.FC = () => {
+	/* Var used to handle the page route to redirect */
+	const [ redirect, setRedirect ] = useState<string>("");
+	/*If User is logged then, redirect to home, if not redirect to slides tutorial*/ 
+	useEffect(() => {
+		/* Initialize the local storage serice */
+		const storageService = new LocalStorageService();
+		// Check if exits jwt in the phone
+		storageService.getItem("jwt")
+			.then((token )=>{
+			/* If exists the jwt in the phone the user is logged */ 
+				if(token.value != null){
+					setRedirect('/home');
+				}else{
+					setRedirect('/slides')
+				}
+			})
+			.catch((err)=>{
+					console.log(err);
+			});
+	}, []);
+	return (
+			/* Finally need to redirect to redirect variable key */
+			<Redirect to={redirect} />
+	)
 };
-
-export default withIonLifeCycle(HomeOrTutorial);
+export default HomeOrLogin;
