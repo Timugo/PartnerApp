af70e348373751b1d9d33c0a9a420314f5634a9b
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-05-01 14:16:44 -0500

    feat(services): improve services performance

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,26 +1,33 @@
 
 import axios from 'axios';
 import { LoginResponse } from "../interfaces/responses.interface"; 
-
-
-const  endpoint  =  `https://sandboxv2.timugo.com`;
+import { Enviroment } from "../enviroments/enviroments";
 
 export class LoginServices {
 
-    async login(user : string , pass:string) {
-      let data :any ={
-        phone: user,
-        password : pass
-      }
-      let config :any = { 
-        headers: {
-         'Content-Type': 'application/json',
-        },
-      }
-      let url : string = `${endpoint}/auth/loginPartner`
-      //fetch the api
-      return  await axios.post<LoginResponse>(url,data,config);
-      
-    }      
+  constructor(private enviroment : Enviroment){}
+  
+  static async login(user : string , pass:string) {
+    /* Initlize the Envoriment Class to get url */
+    let enviroment = new Enviroment();
+    const BASE_URL = await enviroment.getUrl();
+    /* Body of request */
+    let data :any ={
+      phone: user,
+      password : pass
+    }
+    /* Config of request */
+    let config :any = { 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    //bUild the api
+    let url : string = `${BASE_URL}/auth/loginPartner`;
+
+    //fetch the api
+    return  await axios.post<LoginResponse>(url,data,config);
+    
+  }      
   
 }
--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,26 +1,31 @@
 
 import axios from 'axios';
 import { LoginResponse } from "../interfaces/responses.interface"; 
+import { Enviroment } from "../enviroments/enviroments";
 
-const  endpoint  =  `https://sandboxv2.timugo.com`;
 
 
 export class LoginServices {
+   
+  constructor(private envoriment : Enviroment){}
 
-    async login(user : string , pass:string) {
-      let data :any ={
-        phone: user,
-        password : pass
-      }
-      let config :any = { 
-        headers: {
-         'Content-Type': 'application/json',
-        },
-      }
-      let url : string = `${endpoint}/auth/loginPartner`
-      //fetch the api
-      return  await axios.post<LoginResponse>(url,data,config);
-      
-    }      
+  async login(user : string , pass:string) {
+    let data :any ={
+      phone: user,
+      password : pass
+    }
+    let config :any = { 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const baseUrl = await this.envoriment.getUrl();
+
+    let url : string = `${baseUrl}/auth/loginPartner`
+    console.log(url);
+    //fetch the api
+    //return  await axios.post<LoginResponse>(url,data,config);
+    
+  }      
   
 }
