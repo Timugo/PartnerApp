096599325dd57a86e50c87806ddac33915623a78
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-05-01 14:16:23 -0500

    feat(envoriment) : New handler envs class

---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,21 @@
+/*
+  This class wil be used to handle all enviroment
+  variables like API TOkens or URLS
+*/
+export class Enviroment {
+  
+  /*
+    This function detects if the envoriment is production
+    or development and returns the properly API_URL 
+  */
+  async getUrl(){
+    /*read de node Env variable https://create-react-app.dev/docs/adding-custom-environment-variables/#expanding-environment-variables-in-env */
+    if(process.env.NODE_ENV==='production'){
+      //Production mode
+      return 'https://sandboxv2.timugo.com'
+    }else{
+      // local and test url
+      return 'https://sandboxv2.timugo.com'
+    }
+  }
+}
