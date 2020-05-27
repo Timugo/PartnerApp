bed3cb25c76e092104d6db973bde20b3cc552739
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-05-26 22:12:54 -0500

    feat(Products): big improvements in Ux/Ui

--- src/pages/Products/create-product.tsx
+++ src/pages/Products/create-product.tsx
@@ -24,7 +24,7 @@ import {
 } from "@ionic/react";
 import React, { useState } from "react";
 /* Ionic icons from ionic library  */
-import { closeOutline } from "ionicons/icons";
+import { closeOutline, image, imageOutline, checkbox, checkboxOutline } from "ionicons/icons";
 /* Services */
 import { ProductService } from "../../services/product.service";
 /* Capacitor plugins libraries */
@@ -43,7 +43,6 @@ const CreateProduct: React.FC = () => {
   const history = useHistory();
   /* variables used in the page */
   const [description, setDescription] = useState<string>("");
-  //const [ value, setValue ] = useState<number>(1000);
   const [timeArrival, setTimeArrival] = useState<number>(0);
   const [name, setName] = useState<string>("");
   const [characteristics, setCharasteristics] = useState<string>("");
@@ -54,53 +53,53 @@ const CreateProduct: React.FC = () => {
   const [message, setMessage] = useState<string>("");
 
   /*
-  This function handle a submit
-  of a new product
+    This function handle a submit
+    of a new product
   */
   const SendProduct = async () => {
-    // let product: Product = {
-    //   description: description,
-    //   benefits: benefits,
-    //   characteristics: characteristics,
-    //   deliveryDays: timeArrival,
-    //   name: name,
-    //   file: imgData,
-    // };
-
-    let productFormData = new FormData();
-    productFormData.append("description", description);
-    productFormData.append("benefits", benefits);
-    productFormData.append("characteristics", characteristics);
-    productFormData.append("deliveryDays", timeArrival.toString());
-    productFormData.append("name", name);
-    productFormData.append("file", imgData, `product_${name}`);
-    productFormData.append("phone", "123456121"); //need to save temporal fix phone in the phone
-
-    ProductService.createProduct(productFormData)
-      .then((response) => {
-        if (response.status === 200) {
-          if (response.data.response === 2) {
-            setMessage("Genial!!, se creo el producto");
-            setShowToast1(true);
-            //setIdProduct(response.data.content.product["_id"]);
-            history.push({
-              pathname: "/products/Presentation",
-              state: {
-                idProduct: response.data.content.product["_id"],
-              },
-            });
-          } else {
-            setMessage("Error al crear el producto, intenta mas tarde");
-            setShowToast1(true);
-            console.log(response);
+    /* If the image isnt upload yet */
+    if(!imgData){
+      /* SHow and toast with message */
+      setMessage("Debes Cargar una imagen");
+      setShowToast1(true);
+    }else{
+      /* If the img was uploaded */
+      /* Build the product with info */
+      let productFormData = new FormData();
+      productFormData.append("description", description);
+      productFormData.append("benefits", benefits);
+      productFormData.append("characteristics", characteristics);
+      productFormData.append("deliveryDays", timeArrival.toString());
+      productFormData.append("name", name);
+      productFormData.append("file", imgData, `product_${name}`);
+      productFormData.append("phone", "123456121"); //need to save temporal fix phone in the phone
+      /* Make the reuest to create a product */
+      ProductService.createProduct(productFormData)
+        .then((response) => {
+          /* Request success */
+          if (response.status === 200) {
+            /* The product was created! */
+            if (response.data.response === 2) {
+              /* Show a message */
+              setMessage("Genial!!, se creo el producto");
+              setShowToast1(true);
+              /* Send to Create presentation */
+              //history.push(`/products/presentation/${response.data.content.product["id"]}`);
+            } else {
+              setMessage("Error al crear el producto, intenta mas tarde");
+              setShowToast1(true);
+              console.log(response);
+            }
           }
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        setMessage("Error al crear el producto, intenta mas tarde");
-        setShowToast1(true);
-      });
+        })
+        .catch((err) => {
+          console.log(err);
+          setMessage("Error al crear el producto, intenta mas tarde");
+          setShowToast1(true);
+        });
+
+    }
+
   };
   
   /*
@@ -117,162 +116,204 @@ const CreateProduct: React.FC = () => {
       allowEditing: false,
       resultType: CameraResultType.Base64,
     };
-    const image: CameraPhoto = await Camera.getPhoto(configCamera);
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    // if (image.webPath) {
-    //   setMessage(image.webPath);
-    //   //to show Img path
-    //   setImg(image.webPath);
-    //   setImgData(image.webPath);
-    // }
-    if (image.base64String) {
-      let dat = await FileConverter.convertFile(image.base64String,`image/${image.format}`,512);
-      console.log(dat);
-      setImgData(dat);
-    }
+    //const image: CameraPhoto = await Camera.getPhoto(configCamera);
+    Camera.getPhoto(configCamera)
+      .then((image: CameraPhoto)=>{
+        // image.webPath will contain a path that can be set as an image src.
+        // You can access the original file using image.path, which can be
+        // passed to the Filesystem API to read the raw data of the image,
+        // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+
+        /* Web path works to display the image in a img src */
+        // if (image.webPath) {
+        //   setMessage(image.webPath);
+        //   //to show Img path
+        //   setImg(image.webPath);
+        //   setImgData(image.webPath);
+        // }
+        if (image.base64String) {
+          //let dat = await FileConverter.convertFile(image.base64String,`image/${image.format}`,512);
+          FileConverter.convertFile(image.base64String,`image/${image.format}`,512)
+            .then(data =>{
+              console.log(data);
+              setImgData(data);
+            })
+            .catch(err=>{
+              setMessage("Intenta con otro tipo de archivo");
+              setShowToast1(true);      
+            });
+        }
+
+      })
+      .catch(err=>{
+        setMessage("No se detecto ninguna camara");
+        setShowToast1(true);
+      });
+    
+    
   };
 
   return (
-    <IonPage id="homePage">
-      {/* Page Header */}
-      <IonHeader className="ion-no-border">
-        <IonToolbar>
-          <IonTitle>Nuevo Producto</IonTitle>
-        </IonToolbar>
-      </IonHeader>
 
-      {/* Page Content */}
-      <IonContent>
-        {/* Auxiliar toast */}
-        <IonToast
-          isOpen={showToast1}
-          onDidDismiss={() => setShowToast1(false)}
-          message={message}
-          duration={500}/>
+    <form>
+      <IonPage id="homePage">
+        {/* Begin Page Header */}
+        <IonHeader className="ion-no-border">
+          <IonToolbar>
+            <IonTitle>Nuevo Producto</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        {/* End of Page Header */}
+        {/* Page Content */}
+        <IonContent>
+          {/* define ionic grid */}
+          <IonGrid>
+            {/* First Row */}
+            <IonRow>
+              <IonCol offset="1">
+                <div className="ion-characteristics-start">
+                  <h4>Crear Producto</h4>
+                  <IonText color="medium">
+                    Añade la informacion
+                  </IonText>
+                </div>
+              </IonCol>
+            </IonRow>
 
-        {/* define ionic grid */}
-        <IonGrid>
-          {/* First Row */}
-          <IonRow>
-            <IonCol offset="1">
-              <div className="ion-characteristics-start">
-                <h4>Crear Producto</h4>
-                <IonText color="medium">
-                  Añade la informacion del producto que quieres crear
-                </IonText>
-              </div>
-            </IonCol>
-          </IonRow>
-          {/* Second row */}
-          <IonRow>
-            <IonCol offset="4" size="4">
-              <IonImg src={img}></IonImg>
-            </IonCol>
-            <IonCol offset="3" size="6">
-              <IonButton size="small" onClick={PickPicture}>
-                {" "}
-                Seleccionar fotografia
-              </IonButton>
-            </IonCol>
-          </IonRow>
-          {/* Row 3  */}
-          <IonRow>
-            <IonCol size="6">
-              <IonItem>
-                <IonLabel position="stacked">Tiempo de entrega</IonLabel>
-                <IonInput
-                  type="number"
-                  className="inputs"
-                  autofocus={true}
-                  value={timeArrival}
-                  placeholder="Dias"
-                  onIonChange={(e) => setTimeArrival(parseInt(e.detail.value!))}
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          {/* Row 4  */}
-          <IonRow>
-            <IonCol>
-              <IonList>
+            {/* Second row */}
+            <IonRow>
+              {/* <IonCol offset="4" size="4">
+                <IonImg src={img}></IonImg>
+              </IonCol> */}
+
+              <IonCol size="6">
                 <IonItem>
-                  <IonLabel position="stacked">Nombre</IonLabel>
+                  <IonLabel position="stacked">Imagen</IonLabel>
+                  <IonButton fill="outline" className={!imgData ? "" : "ion-hide"} onClick={PickPicture}>
+                    <IonIcon slot="start" icon={imageOutline} />
+                    Cargar
+                  </IonButton>
+                    <IonText color="medium" className={imgData ? "" : "ion-hide"}>
+                    <div>
+                      {`Cargada  `}
+                      <IonIcon color="primary" slot="end" icon={checkboxOutline} />
+                    </div>
+                    </IonText>
+                </IonItem>
+              </IonCol>
+              <IonCol size="6">
+                <IonItem>
+                  <IonLabel position="stacked">Tiempo de entrega</IonLabel>
                   <IonInput
+                    type="number"
                     className="inputs"
                     autofocus={true}
-                    value={name}
-                    placeholder="Nombre del producto"
-                    onIonChange={(e) => setName(e.detail.value!)}
-                  ></IonInput>
+                    value={timeArrival}
+                    required={true}
+                    placeholder="Dias"
+                    onIonChange={(e) => setTimeArrival(parseInt(e.detail.value!))}>
+                  </IonInput>
                 </IonItem>
-                <IonItem>
-                  <IonLabel position="stacked">Descripcion</IonLabel>
-                  <IonInput
-                    className="inputs"
-                    value={description}
-                    placeholder="Descripcion del Producto"
-                    onIonChange={(e) => setDescription(e.detail.value!)}
-                  ></IonInput>
-                </IonItem>
-                <IonItem>
-                  <IonLabel position="stacked">Caracteriticas</IonLabel>
-                  <IonTextarea
-                    className="inputs"
-                    placeholder="Caracteristicas de tu producto (maximo 400 palabras)"
-                    autoGrow={true}
-                    maxlength={400}
-                    value={characteristics}
-                    rows={6}
-                    cols={20}
-                    onIonChange={(e) => setCharasteristics(e.detail.value!)}
-                  ></IonTextarea>
-                </IonItem>
-                <IonItem>
-                  <IonLabel position="stacked">Beneficios</IonLabel>
-                  <IonTextarea
-                    className="inputs"
-                    placeholder="Ejemplo : Aporta nutrientes escenciales ....... (opcional)"
-                    autoGrow={true}
-                    maxlength={400}
-                    value={benefits}
-                    rows={6}
-                    cols={20}
-                    onIonChange={(e) => setBenefits(e.detail.value!)}
-                  ></IonTextarea>
-                </IonItem>
-              </IonList>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonContent>
+              </IonCol>
+            </IonRow>
+            {/* Row 3  */}
+            <IonRow>
+             
+            </IonRow>
+            {/* Row 4  */}
+            <IonRow>
+              <IonCol>
+                <IonList>
+                  <IonItem>
+                    <IonLabel position="stacked">Nombre</IonLabel>
+                    <IonInput
+                      className="inputs"
+                      autofocus={true}
+                      value={name}
+                      placeholder="Nombre del producto"
+                      required={true}
+                      onIonChange={(e) => setName(e.detail.value!)}>
+                    </IonInput>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel position="stacked">Descripcion</IonLabel>
+                    <IonInput
+                      className="inputs"
+                      value={description}
+                      placeholder="Descripcion del Producto"
+                      required={true}
+                      onIonChange={(e) => setDescription(e.detail.value!)}>
+                    </IonInput>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel position="stacked">Caracteriticas</IonLabel>
+                    <IonTextarea
+                      className="inputs"
+                      placeholder="Caracteristicas de tu producto (maximo 400 palabras)"
+                      autoGrow={true}
+                      maxlength={400}
+                      value={characteristics}
+                      rows={6}
+                      cols={20}
+                      required={true}
+                      onIonChange={(e) => setCharasteristics(e.detail.value!)}>
+                    </IonTextarea>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel position="stacked">Beneficios</IonLabel>
+                    <IonTextarea
+                      className="inputs"
+                      placeholder="Ejemplo : Aporta nutrientes escenciales ....... (opcional)"
+                      autoGrow={true}
+                      maxlength={400}
+                      value={benefits}
+                      rows={6}
+                      cols={20}
+                      required={true}
+                      onIonChange={(e) => setBenefits(e.detail.value!)}>
+                    </IonTextarea>
+                  </IonItem>
+                </IonList>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
 
-      {/* Define bottom of the page ionic footer */}
-      <IonFooter className="ButtonFooter">
-        <IonGrid>
-          <IonRow className="ion-align-items-end ion-justify-content-between">
-            <IonCol size="6">
-              <IonButton
-                href="/tabs"
-                className="cancelButton"
-                expand="block"
-                fill="clear"
-              >
-                <IonIcon icon={closeOutline} slot="start" />
-                Cancelar
-              </IonButton>
-            </IonCol>
-            <IonCol size="6">
-              <IonButton expand="block" onClick={SendProduct}>
-                Crear
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonFooter>
-    </IonPage>
+          {/* Auxiliar Components */}
+          <IonToast
+            isOpen={showToast1}
+            onDidDismiss={() => setShowToast1(false)}
+            message={message}
+            color="primary"
+            duration={600}/>
+          {/* End Of auxiliary components */}
+        </IonContent>
+        {/* End of page content */}
+        {/* Begin footer page */}
+        <IonFooter className="ButtonFooter">
+          <IonGrid>
+            <IonRow className="ion-align-items-end ion-justify-content-between">
+              <IonCol size="6">
+                <IonButton
+                  href="/tabs"
+                  className="cancelButton"
+                  expand="block"
+                  fill="clear">
+                  <IonIcon icon={closeOutline} slot="start" />
+                  Cancelar
+                </IonButton>
+              </IonCol>
+              <IonCol size="6">
+                <IonButton type="submit" expand="block" onClick={SendProduct}>
+                  Crear
+                </IonButton>
+              </IonCol>
+            </IonRow>
+            
+          </IonGrid>
+        </IonFooter>
+        {/* End of footer page */}
+      </IonPage>
+    </form>
   );
 };
 
